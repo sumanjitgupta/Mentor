f54cc7022f57d456a4f99504e385a9116a7adcce
I323847 <I323847@AGRN50940928A.apj.global.corp.sap>
2020-07-04 14:39:39 +0530

    4th july - Sumanjit

---
+++ gmentor/src/com/java/model/course.java
@@ -0,0 +1,43 @@
+package com.java.model;
+
+public class course {
+	
+	private String coursename;
+	private String timeline;
+	private String details;
+	private String link;
+	private String summary;
+	
+	
+	public String getCoursename() {
+		return coursename;
+	}
+	public void setCoursename(String coursename) {
+		this.coursename = coursename;
+	}
+	public String getTimeline() {
+		return timeline;
+	}
+	public void setTimeline(String timeline) {
+		this.timeline = timeline;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+}
